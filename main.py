248977c52e75ab37a2f03310e961cdb2a4a330f6
William <r2010shadow@163.com>
2023-08-29 17:49:10 +0800

    Update main.py

--- main.py
+++ main.py
@@ -91,15 +91,26 @@ if cola:
 
 
 #  9
-import pandas_profiling
-from streamlit_pandas_profiling import st_profile_report
+#import pandas as pd
+#import pandas_profiling
+#from streamlit_pandas_profiling import st_profile_report
 
-st.header('`streamlit_pandas_profiling`')
+#st.header('`streamlit_pandas_profiling`')
 
-df = pd.read_csv('https://raw.githubusercontent.com/dataprofessor/data/master/penguins_cleaned.csv')
+#df = pd.read_csv('https://raw.githubusercontent.com/dataprofessor/data/master/penguins_cleaned.csv')
 
-pr = df.profile_report()
-st_profile_report(pr)
+#pr = df.profile_report()
+#st_profile_report(pr)
+
+# 10
+st.header('st.latex')
+
+st.latex(r'''
+     a + ar + a r^2 + a r^3 + \cdots + a r^{n-1} =
+     \sum_{k=0}^{n-1} ar^k =
+     a \left(\frac{1-r^{n}}{1-r}\right)
+     ''')
+
 
 
 
