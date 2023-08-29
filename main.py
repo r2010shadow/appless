033d811031e38a98bc811aa803fe2f7a71279d21
William <r2010shadow@163.com>
2023-08-29 17:25:49 +0800

    Update main.py

--- main.py
+++ main.py
@@ -71,12 +71,27 @@ options = st.multiselect(
 
 st.write('You selected:', options)
 
-#
+# 8
+
+st.header('st.checkbox')
+
+st.write ('What would you like to order?')
+
+icecream = st.checkbox('Ice cream')
+coffee = st.checkbox('Coffee')
+cola = st.checkbox('Cola')
+
+if icecream:
+     st.write("Great! Here's some more 🍦")
+
+if coffee:
+     st.write("Okay, here's some coffee ☕")
+
+if cola:
+     st.write("Here you go 🥤")
 
 
-
-
-
+# 
 
 
 
