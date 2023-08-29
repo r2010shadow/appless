e3560d8d3eed294264b14fc49c8255903e6c12e3
William <r2010shadow@163.com>
2023-08-29 17:22:48 +0800

    Update main.py

--- main.py
+++ main.py
@@ -60,3 +60,23 @@ option = st.selectbox(
      ('Blue', 'Red', 'Green'))
 
 st.write('Your favorite color is ', option)
+
+# 7
+st.header('st.multiselect')
+
+options = st.multiselect(
+     'What are your favorite colors',
+     ['Green', 'Yellow', 'Red', 'Blue'],
+     ['Yellow', 'Red'])
+
+st.write('You selected:', options)
+
+#
+
+
+
+
+
+
+
+
