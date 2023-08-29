0321dacfc37d2617d84b654e60fc041b040154af
William <r2010shadow@163.com>
2023-08-29 13:59:13 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
 import streamlit as st
 
 # Add title and Write in
+
 st.title('That is my app.')
 st.write("Streamlit is an awesome tools.")
+
+import streamlit as st
+number = st.slider("Pick a number: ", min_value=1, max_value=10)
+st.text("Your number is " + str(number))
