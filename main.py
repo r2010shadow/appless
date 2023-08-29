2f0f3800860eeaec133966394ae45336f103b949
William <r2010shadow@163.com>
2023-08-29 15:37:38 +0800

    Updated

--- main.py
+++ main.py
@@ -8,3 +8,43 @@ st.write("Streamlit is an awesome tools.")
 import streamlit as st
 number = st.slider("Pick a number: ", min_value=1, max_value=10)
 st.text("Your number is " + str(number))
+
+
+from datetime import time, datetime
+
+st.header('st.slider')
+
+# 样例 1
+
+st.subheader('Slider')
+
+age = st.slider('How old are you?', 0, 130, 25)
+st.write("I'm ", age, 'years old')
+
+# 样例 2
+
+st.subheader('Range slider')
+
+values = st.slider(
+     'Select a range of values',
+     0.0, 100.0, (25.0, 75.0))
+st.write('Values:', values)
+
+# 样例 3
+
+st.subheader('Range time slider')
+
+appointment = st.slider(
+     "Schedule your appointment:",
+     value=(time(11, 30), time(12, 45)))
+st.write("You're scheduled for:", appointment)
+
+# 样例 4
+
+st.subheader('Datetime slider')
+
+start_time = st.slider(
+     "When do you start?",
+     value=datetime(2020, 1, 1, 9, 30),
+     format="MM/DD/YY - hh:mm")
+st.write("Start time:", start_time)
