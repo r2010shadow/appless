93003fb939aa890064392dce9630bccfa901ab6b
William <r2010shadow@163.com>
2023-08-30 16:49:07 +0800

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,10 @@ import streamlit as st
 from datetime import time, datetime
 import time
 
+## one line cartoon tips ##
+# st.balloons() 
+## ##
+
 # 14
 
 st.set_page_config(layout="wide")
@@ -180,10 +184,6 @@ else:
 
 # 15
 st.title('st.progress')
-
-with st.expander('About this app'):
-     st.write('You can now display the progress of your calculations in a Streamlit app with the `st.progress` command.')
-
 my_bar = st.progress(0)
 
 for percent_complete in range(100):
@@ -192,7 +192,52 @@ for percent_complete in range(100):
 
 st.balloons()
 
-#
+# 16
+st.title('st.form')
+
+# Full example of using the with notation
+st.header('1. Example of using `with` notation')
+st.subheader('Coffee machine')
+
+with st.form('my_form'):
+    st.subheader('**Order your coffee**')
+
+    # Input widgets
+    coffee_bean_val = st.selectbox('Coffee bean', ['Arabica', 'Robusta'])
+    coffee_roast_val = st.selectbox('Coffee roast', ['Light', 'Medium', 'Dark'])
+    brewing_val = st.selectbox('Brewing method', ['Aeropress', 'Drip', 'French press', 'Moka pot', 'Siphon'])
+    serving_type_val = st.selectbox('Serving format', ['Hot', 'Iced', 'Frappe'])
+    milk_val = st.select_slider('Milk intensity', ['None', 'Low', 'Medium', 'High'])
+    owncup_val = st.checkbox('Bring own cup')
+
+    # Every form must have a submit button
+    submitted = st.form_submit_button('Submit')
+
+if submitted:
+    st.markdown(f'''
+        ☕ You have ordered:
+        - Coffee bean: `{coffee_bean_val}`
+        - Coffee roast: `{coffee_roast_val}`
+        - Brewing: `{brewing_val}`
+        - Serving type: `{serving_type_val}`
+        - Milk: `{milk_val}`
+        - Bring own cup: `{owncup_val}`
+        ''')
+else:
+    st.write('☝️ Place your order!')
+
+
+# Short example of using an object notation
+st.header('2. Example of object notation')
+
+form = st.form('my_form_2')
+selected_val = form.slider('Select a value')
+form.form_submit_button('Submit')
+
+st.write('Selected value: ', selected_val)
+
+
+# 17
 
 
 
