cb48130911ac5afe8f03e337ab4174bd8620855e
William <r2010shadow@163.com>
2023-08-30 11:16:16 +0800

    Update main.py

--- main.py
+++ main.py
@@ -133,7 +133,7 @@ st.write('Selected number from slider widget is:', number)
 #st.title('st.secrets')
 #st.write(st.secrets['message'])
 
-#13
+#13 notepad -> save *.csv(utf8)
 st.title('st.file_uploader')
 
 st.subheader('Input CSV')
@@ -147,5 +147,56 @@ if uploaded_file is not None:
   st.write(df.describe())
 else:
   st.info('☝️ Upload a CSV file')
+
+# 14
+
+st.set_page_config(layout="wide")
+
+st.title('How to layout your Streamlit app')
+
+with st.expander('About this app'):
+  st.write('This app shows the various ways on how you can layout your Streamlit app.')
+  st.image('https://streamlit.io/images/brand/streamlit-logo-secondary-colormark-darktext.png', width=250)
+
+st.sidebar.header('Input')
+user_name = st.sidebar.text_input('What is your name?')
+user_emoji = st.sidebar.selectbox('Choose an emoji', ['', '😄', '😆', '😊', '😍', '😴', '😕', '😱'])
+user_food = st.sidebar.selectbox('What is your favorite food?', ['', 'Tom Yum Kung', 'Burrito', 'Lasagna', 'Hamburger', 'Pizza'])
+
+st.header('Output')
+
+col1, col2, col3 = st.columns(3)
+
+with col1:
+  if user_name != '':
+    st.write(f'👋 Hello {user_name}!')
+  else:
+    st.write('👈  Please enter your **name**!')
+
+with col2:
+  if user_emoji != '':
+    st.write(f'{user_emoji} is your favorite **emoji**!')
+  else:
+    st.write('👈 Please choose an **emoji**!')
+
+with col3:
+  if user_food != '':
+    st.write(f'🍴 **{user_food}** is your favorite **food**!')
+  else:
+    st.write('👈 Please choose your favorite **food**!')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
