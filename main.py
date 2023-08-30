80a0ca5097fe827698cd56006f43c5e117f16103
William <r2010shadow@163.com>
2023-08-30 10:40:12 +0800

    Update main.py

--- main.py
+++ main.py
@@ -133,5 +133,19 @@ st.write('Selected number from slider widget is:', number)
 #st.title('st.secrets')
 #st.write(st.secrets['message'])
 
+#13
+st.title('st.file_uploader')
+
+st.subheader('Input CSV')
+uploaded_file = st.file_uploader("Choose a file")
+
+if uploaded_file is not None:
+  df = pd.read_csv(uploaded_file)
+  st.subheader('DataFrame')
+  st.write(df)
+  st.subheader('Descriptive Statistics')
+  st.write(df.describe())
+else:
+  st.info('☝️ Upload a CSV file')
 
 
