cd9ae9d2c975cf205cba8828a857419466db0f0f
William <r2010shadow@163.com>
2023-09-04 17:48:41 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-
 import streamlit as st
+import pandas as pd
 
 col1, col2, col3, col4 = st.columns((2,1,1,1))
 
@@ -12,4 +12,12 @@ with col3:
 with col4:
     st.write("Now in write zone")
 
+@st.cache_data
+def load_data(url):
+    df = pd.read_csv(url)  # 👈 Download the data
+    return df
 
+df = load_data("https://github.com/r2010shadow/appless/blob/main/main.py")
+st.dataframe(df)
+
+st.button("Rerun")
