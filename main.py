c60dec6aa341b516299ce54e23b670d84d3f6ad3
William <r2010shadow@163.com>
2023-09-04 18:05:35 +0800

    Update main.py

--- main.py
+++ main.py
@@ -22,7 +22,7 @@ st.title('Uber pickups in NYC')
 
 DATE_COLUMN = 'date/time'
 #DATA_URL = ('https://s3-us-west-2.amazonaws.com/'
-            'streamlit-demo-data/uber-raw-data-sep14.csv.gz')
+#            'streamlit-demo-data/uber-raw-data-sep14.csv.gz')
 DATA_URL = df
 
 @st.cache_data
