f69238c95b6c1b42db73a1800ddf14f219929a1a
William <r2010shadow@163.com>
2023-09-04 17:51:25 +0800

    Update main.py

--- main.py
+++ main.py
@@ -4,13 +4,13 @@ import pandas as pd
 #col1, col2, col3, col4 = st.columns((2,1,1,1))
 
 
-    st.title("This is my first app")
+st.title("This is my first app")
 
-    st.header("This is header")
+st.header("This is header")
 
-    st.subheader("This is subheader")
+st.subheader("This is subheader")
 
-    st.write("Now in write zone")
+st.write("Now in write zone")
 
 
 df = load_data("https://github.com/r2010shadow/appless/blob/main/main.py")
