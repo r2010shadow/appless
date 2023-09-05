7b44f9d0485b3a7dd0812f0588a35f233072246f
William <r2010shadow@163.com>
2023-09-05 09:54:40 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import streamlit as st
-
+import request
 
 st.write("loading ...")
-file = ("https://github.com/r2010shadow/appless/blob/main/upload-case.py")
+file = request.get("https://github.com/r2010shadow/appless/blob/main/upload-case.py")
 st.write(file)
