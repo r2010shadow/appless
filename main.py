e24e98333110c7a4dfdde818d0f2cc72cefdd218
William <r2010shadow@163.com>
2023-09-12 14:42:29 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,383 @@
+# code from 30days.streamlit.app
 import streamlit as st
+from datetime import time, datetime
+import time
+
+## one line cartoon tips ##
+# st.balloons() 
+## ##
+
+# 14
+
+st.set_page_config(layout="wide")
+
+#col11, col12, col13, col14 = st.columns((2,1,1,1))
 
 
-st.write("loading ...")
-st.txt("file")
+st.title('How to layout your Streamlit app')
+
+with st.expander('About this app'):
+  #st.write('This app shows the various ways on how you can layout your Streamlit app.')
+  st.image('https://www.labware.com/hs-fs/hubfs/_LabWare.com/Logos/LabWare%20Corporate%20Logo%20Color.png?width=250&height=143&name=LabWare%20Corporate%20Logo%20Color.png', width=250)
+
+  st.sidebar.header('Input')
+  user_name = st.sidebar.text_input('What is your name?')
+  user_emoji = st.sidebar.selectbox('Choose an emoji', ['', '😄', '😆', '😊', '😍', '😴', '😕', '😱'])
+  user_food = st.sidebar.selectbox('What is your favorite food?', ['', 'Tom Yum Kung', 'Burrito', 'Lasagna', 'Hamburger', 'Pizza'])
+  st.header('Output')
+
+col1, col2, col3 = st.columns(3)
+
+with col1:
+  if user_name != '':
+    st.write(f'👋 Hello {user_name}!')
+  else:
+    st.write('👈  Please enter your **name**!')
+
+with col2:
+  if user_emoji != '':
+    st.write(f'{user_emoji} is your favorite **emoji**!')
+  else:
+    st.write('👈 Please choose an **emoji**!')
+
+with col3:
+  if user_food != '':
+    st.write(f'🍴 **{user_food}** is your favorite **food**!')
+  else:
+    st.write('👈 Please choose your favorite **food**!')
+
+c11, c12 = st.columns(2)
+# 样例 1
+with c11:
+  st.subheader('Slider')
+  age = st.slider('How old are you?', 0, 130, 25)
+  st.write("I'm ", age, 'years old')
+with c12:
+# 样例 2
+  st.subheader('Range slider')
+  values = st.slider(
+     'Select a range of values',
+     0.0, 100.0, (25.0, 75.0))
+  st.write('Values:', values)
+
+
+# 样例 3
+  st.header('st.button')
+  if st.button('Say hello'):
+     st.write('Why hello there')
+  else:
+     st.write('Goodbye')
+
+# 样例 4
+
+st.subheader('Datetime slider')
+
+start_time = st.slider(
+     "When do you start?",
+     value=datetime(2020, 1, 1, 9, 30),
+     format="MM/DD/YY - hh:mm")
+st.write("Start time:", start_time)
+# 5
+import pandas as pd
+import numpy as np
+
+st.header('Line chart')
+#chart_data = pd.DataFrame(
+#     np.random.randn(20, 3),
+#     columns=['a', 'b', 'c'])
+#st.line_chart(chart_data)
+
+# 6
+st.header('st.selectbox')
+option = st.selectbox(
+     'What is your favorite color?',
+     ('Blue', 'Red', 'Green'))
+
+st.write('Your favorite color is ', option)
+
+# 7
+st.header('st.multiselect')
+
+options = st.multiselect(
+     'What are your favorite colors',
+     ['Green', 'Yellow', 'Red', 'Blue'],
+     ['Yellow', 'Red'])
+
+st.write('You selected:', options)
+
+# 8
+st.header('st.checkbox')
+
+st.write ('What would you like to order?')
+
+icecream = st.checkbox('Ice cream')
+coffee = st.checkbox('Coffee')
+cola = st.checkbox('Cola')
+
+if icecream:
+     st.write("Great! Here's some more 🍦")
+
+if coffee:
+     st.write("Okay, here's some coffee ☕")
+
+if cola:
+     st.write("Here you go 🥤")
+
+
+#  9
+#import pandas_profiling
+#from streamlit_pandas_profiling import st_profile_report
+
+#st.header('`streamlit_pandas_profiling`')
+
+#df = pd.read_csv('https://raw.githubusercontent.com/dataprofessor/data/master/penguins_cleaned.csv')
+
+#pr = df.profile_report()
+#st_profile_report(pr)
+
+# 10
+st.header('st.latex')
+
+st.latex(r'''
+     a + ar + a r^2 + a r^3 + \cdots + a r^{n-1} =
+     \sum_{k=0}^{n-1} ar^k =
+     a \left(\frac{1-r^{n}}{1-r}\right)
+     ''')
+
+# 11
+st.title('Customizing the theme of Streamlit apps')
+
+st.write('Contents of the `.streamlit/config.toml` file of this app')
+
+st.code("""
+[theme]
+primaryColor="#F39C12"
+backgroundColor="#2E86C1"
+secondaryBackgroundColor="#AED6F1"
+textColor="#FFFFFF"
+font="monospace"
+""")
+
+number = st.sidebar.slider('Select a number:', 0, 10, 5)
+st.write('Selected number from slider widget is:', number)
+
+
+# 12
+#st.title('st.secrets')
+#st.write(st.secrets['message'])
+
+#13 notepad -> save *.csv(utf8)
+st.title('st.file_uploader')
+
+st.subheader('Input CSV')
+uploaded_file = st.file_uploader("Choose a file")
+
+if uploaded_file is not None:
+  df = pd.read_csv(uploaded_file)
+  st.subheader('DataFrame')
+  st.write(df)
+  st.subheader('Descriptive Statistics')
+  st.write(df.describe())
+else:
+  st.info('☝️ Upload a CSV file')
+
+
+# 15
+#st.title('st.progress')
+#my_bar = st.progress(0)
+
+#for percent_complete in range(100):
+#     time.sleep(0.05)
+#     my_bar.progress(percent_complete + 1)
+
+#st.balloons()
+
+# 16
+st.title('st.form')
+
+# Full example of using the with notation
+st.header('1. Example of using `with` notation')
+st.subheader('Coffee machine')
+
+with st.form('my_form'):
+    st.subheader('**Order your coffee**')
+
+    # Input widgets
+    coffee_bean_val = st.selectbox('Coffee bean', ['Arabica', 'Robusta'])
+    coffee_roast_val = st.selectbox('Coffee roast', ['Light', 'Medium', 'Dark'])
+    brewing_val = st.selectbox('Brewing method', ['Aeropress', 'Drip', 'French press', 'Moka pot', 'Siphon'])
+    serving_type_val = st.selectbox('Serving format', ['Hot', 'Iced', 'Frappe'])
+    milk_val = st.select_slider('Milk intensity', ['None', 'Low', 'Medium', 'High'])
+    owncup_val = st.checkbox('Bring own cup')
+
+    # Every form must have a submit button
+    submitted = st.form_submit_button('Submit')
+
+if submitted:
+    st.markdown(f'''
+        ☕ You have ordered:
+        - Coffee bean: `{coffee_bean_val}`
+        - Coffee roast: `{coffee_roast_val}`
+        - Brewing: `{brewing_val}`
+        - Serving type: `{serving_type_val}`
+        - Milk: `{milk_val}`
+        - Bring own cup: `{owncup_val}`
+        ''')
+else:
+    st.write('☝️ Place your order!')
+
+
+# Short example of using an object notation
+st.header('2. Example of object notation')
+
+form = st.form('my_form_2')
+selected_val = form.slider('Select a value')
+form.form_submit_button('Submit')
+
+st.write('Selected value: ', selected_val)
+
+
+# 17
+# Using cache
+
+
+# 18
+st.title('st.session_state')
+
+def lbs_to_kg():
+  st.session_state.kg = st.session_state.lbs/2.2046
+def kg_to_lbs():
+  st.session_state.lbs = st.session_state.kg*2.2046
+
+st.header('Input')
+col1, spacer, col2 = st.columns([2,1,2])
+with col1:
+  pounds = st.number_input("Pounds:", key = "lbs", on_change = lbs_to_kg)
+with col2:
+  kilogram = st.number_input("Kilograms:", key = "kg", on_change = kg_to_lbs)
+
+st.header('Output')
+st.write("st.session_state object:", st.session_state)
+
+# 19
+import requests
+
+#st.title('🏀 Bored API app')
+
+st.sidebar.header('Input')
+selected_type = st.sidebar.selectbox('Select an activity type', ["education", "recreational", "social", "diy", "charity", "cooking", "relaxation", "music", "busywork"])
+
+suggested_activity_url = f'http://www.boredapi.com/api/activity?type={selected_type}'
+json_data = requests.get(suggested_activity_url)
+suggested_activity = json_data.json()
+
+c1, c2 = st.columns(2)
+with c1:
+  with st.expander('About this app'):
+    st.write('Are you bored? The **Bored API app** provides suggestions on activities that you can do when you are bored. This app is powered by the Bored API.')
+with c2:
+  with st.expander('JSON data'):
+    st.write(suggested_activity)
+
+st.header('Suggested activity')
+st.info(suggested_activity['activity'])
+
+col1, col2, col3 = st.columns(3)
+with col1:
+  st.metric(label='Number of Participants', value=suggested_activity['participants'], delta='')
+with col2:
+  st.metric(label='Type of Activity', value=suggested_activity['type'].capitalize(), delta='')
+with col3:
+  st.metric(label='Price', value=suggested_activity['price'], delta='')
+
+# 20
+# From https://blog.streamlit.io/how-to-build-streamlit-apps-on-replit/
+##
+
+# Expander section
+with st.expander("About"):
+  st.write("""Trying to add a data table, chart, sidebar button with 
+          ballons, an image, text input & exploring tabs!""")
+
+# Sidebar section
+with st.sidebar:
+  st.subheader('This is a Sidebar')
+  st.write('Button with Balloons 🎈')
+  if st.button('Click me!✨'):
+    st.balloons()
+  else:
+    st.write(' ')
+
+# Dataframe and Chart display section
+st.subheader('Interactive Data Table')
+df = pd.DataFrame(
+    np.random.randn(50, 3),  # generates random numeric values!
+    columns=["a", "b", "c"])
+st.dataframe(df)
+
+st.subheader('Bar Chart 📊')
+st.bar_chart(df)
+
+# Image upload and text input section
+#st.subheader('An Image')
+st.image(
+    'https://www.labware.com/hs-fs/hubfs/_LabWare.com/Logos/LabWare%20Corporate%20Logo%20Color.png?width=250&height=143&name=LabWare%20Corporate%20Logo%20Color.png', width=250)
+
+st.subheader('Text Input')
+greet = st.text_input('Write your name, please!')
+st.write('👋 Hey!', greet)
+
+# Tabs section
+st.subheader('Tabs')
+tab1, tab2 = st.tabs(["TAB 1", "TAB 2"])
+
+with tab1:
+  st.write('WOW!')
+  #st.image("https://img.zcool.cn/community/017bc85fc0cdbb11013fdcc78f8de1.gif", width=200)
+
+with tab2:
+  st.write('Do you like ice cream? 🍨')
+  agree = st.checkbox('Yes! I love it')
+  disagree = st.checkbox("Nah! 😅")
+  if agree:
+    st.write('Even I love it 🤤')
+  if disagree:
+    st.write('You are boring 😒')
+
+st.subheader("Load data from gz")
+import pandas as pd
+import numpy as np
+
+st.title('Uber pickups in NYC')
+
+DATE_COLUMN = 'date/time'
+#DATA_URL = ('https://s3-us-west-2.amazonaws.com/'
+#            'streamlit-demo-data/uber-raw-data-sep14.csv.gz')
+DATA_URL = df
+
+@st.cache_data
+def load_data(nrows):
+    data = pd.read_csv(DATA_URL, nrows=nrows)
+    lowercase = lambda x: str(x).lower()
+    data.rename(lowercase, axis='columns', inplace=True)
+    data[DATE_COLUMN] = pd.to_datetime(data[DATE_COLUMN])
+    return data
+
+data_load_state = st.text('Loading data...')
+data = load_data(10000)
+data_load_state.text("Done! (using st.cache_data)")
+
+if st.checkbox('Show raw data'):
+    st.subheader('Raw data')
+    st.write(data)
+
+st.subheader('Number of pickups by hour')
+hist_values = np.histogram(data[DATE_COLUMN].dt.hour, bins=24, range=(0,24))[0]
+st.bar_chart(hist_values)
+
+# Some number in the range 0-23
+hour_to_filter = st.slider('hour', 0, 23, 17)
+filtered_data = data[data[DATE_COLUMN].dt.hour == hour_to_filter]
+
+st.subheader('Map of all pickups at %s:00' % hour_to_filter)
+st.map(filtered_data)
